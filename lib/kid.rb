09394a19e6c2ba8a237e0dace95e5106eb2f2de0
Nicholas Ng <nicholasykng@gmail.com>
2019-11-02 22:17:54 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,9 +1,5 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 class Kid
-  include Dance
-  attr_accessor :name
-  def initialize(name)
-    @name = name
+  extend MetaDancing
   end
-end
